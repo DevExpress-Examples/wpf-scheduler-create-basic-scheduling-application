29b6e53bebc303ebdf880d4b7253cad30b19db7d
dxgoncharov <36544836+dxgoncharov@users.noreply.github.com>
2021-10-04 14:05:41 +0300

    Update Doctor.cs

--- CS/SimpleSchedulingExample/Doctor.cs
+++ CS/SimpleSchedulingExample/Doctor.cs
@@ -1,22 +1,16 @@
-﻿using DevExpress.Mvvm.POCO;
-
-namespace SimpleSchedulingExample {
-    #region #resource
-    public class Doctor {
-        public static Doctor Create() {
-            return ViewModelSource.Create(() => new Doctor());
-        }
-        public static Doctor Create(int Id, string Name) {
-            Doctor doctor = Doctor.Create();
-            doctor.Id = Id;
-            doctor.Name = Name;
-            return doctor;
-        }
-
-        protected Doctor() { }
-
-        public virtual int Id { get; set; }
-        public virtual string Name { get; set; }
+public class Doctor {
+    public static Doctor Create() {
+        return new Doctor();
     }
-    #endregion #resource
+    public static Doctor Create(int Id, string Name) {
+        Doctor doctor = Doctor.Create();
+        doctor.Id = Id;
+        doctor.Name = Name;
+        return doctor;
+    }
+
+    protected Doctor() { }
+
+    public virtual int Id { get; set; }
+    public virtual string Name { get; set; }
 }
