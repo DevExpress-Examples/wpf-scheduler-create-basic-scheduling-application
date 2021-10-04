cd540638a9b607cebe0294fd0e6b04ed102134d0
DevExpressExampleBot <fakeEmail>
2021-10-04 14:08:31 +0300

    Source auto update [skip ci]

--- VB/SimpleSchedulingExample/MainViewModel.vb
+++ VB/SimpleSchedulingExample/MainViewModel.vb
@@ -1,26 +1,28 @@
-﻿#Region "#viewmodel"
-Imports System
 Imports System.Collections.ObjectModel
 
 Namespace SimpleSchedulingExample
-	Public Class MainViewModel
-		Public Overridable Property Doctors() As ObservableCollection(Of Doctor)
-		Public Overridable Property Appointments() As ObservableCollection(Of MedicalAppointment)
 
-		Protected Sub New()
-			CreateDoctors()
-			CreateMedicalAppointments()
-		End Sub
-		Private Sub CreateDoctors()
-			Doctors = New ObservableCollection(Of Doctor)()
-			Doctors.Add(Doctor.Create(Id:= 1, Name:= "Stomatologist"))
-			Doctors.Add(Doctor.Create(Id:= 2, Name:= "Ophthalmologist"))
-			Doctors.Add(Doctor.Create(Id:= 3, Name:= "Surgeon"))
-		End Sub
-		Private Sub CreateMedicalAppointments()
-			Appointments = New ObservableCollection(Of MedicalAppointment)()
-			Appointments.Add(MedicalAppointment.Create(startTime:= DateTime.Now.Date.AddHours(10), endTime:= DateTime.Now.Date.AddHours(11), doctorId:= 1, notes:= "", location:= "101", categoryId:=1, patientName:= "Dave Muriel", insuranceNumber:= "396-36-XXXX", firstVisit:= True))
-		End Sub
-	End Class
+    Public Class MainViewModel
+
+        Public Overridable Property Doctors As ObservableCollection(Of Doctor)
+
+        Public Overridable Property Appointments As ObservableCollection(Of MedicalAppointment)
+
+        Public Sub New()
+            CreateDoctors()
+            CreateMedicalAppointments()
+        End Sub
+
+        Private Sub CreateDoctors()
+            Doctors = New ObservableCollection(Of Doctor)()
+            Doctors.Add(Doctor.Create(Id:=1, Name:="Stomatologist"))
+            Doctors.Add(Doctor.Create(Id:=2, Name:="Ophthalmologist"))
+            Doctors.Add(Doctor.Create(Id:=3, Name:="Surgeon"))
+        End Sub
+
+        Private Sub CreateMedicalAppointments()
+            Appointments = New ObservableCollection(Of MedicalAppointment)()
+            Appointments.Add(MedicalAppointment.Create(startTime:=Date.Now.Date.AddHours(10), endTime:=Date.Now.Date.AddHours(11), doctorId:=1, notes:="", location:="101", categoryId:=1, patientName:="Dave Muriel", insuranceNumber:="396-36-XXXX", firstVisit:=True))
+        End Sub
+    End Class
 End Namespace
-#End Region ' #viewmodel
