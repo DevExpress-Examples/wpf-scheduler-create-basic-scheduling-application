ac90533d409de8ec9998fd81125529463f0d004f
DevExpressExampleBot <fakeEmail>
2022-10-28 10:24:06 +0400

    Source auto update [skip ci]

--- VB/SimpleSchedulingExample/Doctor.vb
+++ VB/SimpleSchedulingExample/Doctor.vb
@@ -1,20 +1,23 @@
-Public Class Doctor
+Namespace SimpleSchedulingExample
 
-    Public Shared Function Create() As Doctor
-        Return New Doctor()
-    End Function
+    Public Class Doctor
 
-    Public Shared Function Create(ByVal Id As Integer, ByVal Name As String) As Doctor
-        Dim doctor As Doctor = Create()
-        doctor.Id = Id
-        doctor.Name = Name
-        Return doctor
-    End Function
+        Public Shared Function Create() As Doctor
+            Return New Doctor()
+        End Function
 
-    Protected Sub New()
-    End Sub
+        Public Shared Function Create(ByVal Id As Integer, ByVal Name As String) As Doctor
+            Dim doctor As Doctor = Create()
+            doctor.Id = Id
+            doctor.Name = Name
+            Return doctor
+        End Function
 
-    Public Overridable Property Id As Integer
+        Protected Sub New()
+        End Sub
 
-    Public Overridable Property Name As String
-End Class
+        Public Overridable Property Id As Integer
+
+        Public Overridable Property Name As String
+    End Class
+End Namespace
