1f683c38b55216d4ad03285e51410c982e68c987
Angelika Andronova <exebeeche@icloud.com>
2022-10-28 10:11:40 +0400

    Update MedicalAppointment.cs
    Adding the namespace

--- CS/SimpleSchedulingExample/MedicalAppointment.cs
+++ CS/SimpleSchedulingExample/MedicalAppointment.cs
@@ -1,42 +1,48 @@
 using System;
 
-public class MedicalAppointment {
-    public static MedicalAppointment Create() {
-        return new MedicalAppointment();
+namespace SimpleSchedulingExample
+{
+    public class MedicalAppointment
+    {
+        public static MedicalAppointment Create()
+        {
+            return new MedicalAppointment();
+        }
+        internal static MedicalAppointment Create(DateTime startTime, DateTime endTime,
+            int doctorId, string notes, string location, int categoryId, string patientName,
+            string insuranceNumber, bool firstVisit)
+        {
+
+            MedicalAppointment apt = MedicalAppointment.Create();
+            apt.StartTime = startTime;
+            apt.EndTime = endTime;
+            apt.DoctorId = doctorId;
+            apt.Notes = notes;
+            apt.Location = location;
+            apt.CategoryId = categoryId;
+            apt.PatientName = patientName;
+            apt.InsuranceNumber = insuranceNumber;
+            apt.FirstVisit = firstVisit;
+            return apt;
+        }
+
+        protected MedicalAppointment() { }
+
+        public virtual int Id { get; set; }
+        public virtual bool AllDay { get; set; }
+        public virtual DateTime StartTime { get; set; }
+        public virtual DateTime EndTime { get; set; }
+        public virtual string PatientName { get; set; }
+        public virtual string Notes { get; set; }
+        public virtual string Subject { get; set; }
+        public virtual int StatusId { get; set; }
+        public virtual int CategoryId { get; set; }
+        public virtual int Type { get; set; }
+        public virtual string Location { get; set; }
+        public virtual string RecurrenceInfo { get; set; }
+        public virtual string ReminderInfo { get; set; }
+        public virtual int? DoctorId { get; set; }
+        public virtual string InsuranceNumber { get; set; }
+        public virtual bool FirstVisit { get; set; }
     }
-    internal static MedicalAppointment Create(DateTime startTime, DateTime endTime,
-        int doctorId, string notes, string location, int categoryId, string patientName,
-        string insuranceNumber, bool firstVisit) {
-
-        MedicalAppointment apt = MedicalAppointment.Create();
-        apt.StartTime = startTime;
-        apt.EndTime = endTime;
-        apt.DoctorId = doctorId;
-        apt.Notes = notes;
-        apt.Location = location;
-        apt.CategoryId = categoryId;
-        apt.PatientName = patientName;
-        apt.InsuranceNumber = insuranceNumber;
-        apt.FirstVisit = firstVisit;
-        return apt;
-    }
-
-    protected MedicalAppointment() { }
-
-    public virtual int Id { get; set; }
-    public virtual bool AllDay { get; set; }
-    public virtual DateTime StartTime { get; set; }
-    public virtual DateTime EndTime { get; set; }
-    public virtual string PatientName { get; set; }
-    public virtual string Notes { get; set; }
-    public virtual string Subject { get; set; }
-    public virtual int StatusId { get; set; }
-    public virtual int CategoryId { get; set; }
-    public virtual int Type { get; set; }
-    public virtual string Location { get; set; }
-    public virtual string RecurrenceInfo { get; set; }
-    public virtual string ReminderInfo { get; set; }
-    public virtual int? DoctorId { get; set; }
-    public virtual string InsuranceNumber { get; set; }
-    public virtual bool FirstVisit { get; set; }
 }
