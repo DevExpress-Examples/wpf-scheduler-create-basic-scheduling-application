895be0d4b1f9a4208c7fc21fca1f96ebfbc52850
DevExpressExampleBot <fakeEmail>
2023-11-29 14:50:09 +0400

    Source auto update [skip ci]

--- VB/SimpleSchedulingExample/Model/Doctor.vb
+++ VB/SimpleSchedulingExample/Model/Doctor.vb
@@ -1,24 +1,27 @@
-﻿#Region "#resource"
+#Region "#resource"
 Imports DevExpress.Mvvm.POCO
 
 Namespace SchedulerViewsExample
+
     Public Class Doctor
+
         Public Shared Function Create() As Doctor
             Return ViewModelSource.Create(Function() New Doctor())
         End Function
-        Public Shared Function Create(ByVal Id As Integer, ByVal Name As String) As Doctor
 
-            Dim doctor_Renamed As Doctor = SchedulerViewsExample.Doctor.Create()
-            doctor_Renamed.Id = Id
-            doctor_Renamed.Name = Name
-            Return doctor_Renamed
+        Public Shared Function Create(ByVal Id As Integer, ByVal Name As String) As Doctor
+            Dim doctor As Doctor = Create()
+            doctor.Id = Id
+            doctor.Name = Name
+            Return doctor
         End Function
 
         Protected Sub New()
         End Sub
 
-        Public Overridable Property Id() As Integer
-        Public Overridable Property Name() As String
+        Public Overridable Property Id As Integer
+
+        Public Overridable Property Name As String
     End Class
 End Namespace
-#End Region ' #resource
+#End Region  ' #resource
--- VB/SimpleSchedulingExample/ViewModel/MainViewModel.vb
+++ VB/SimpleSchedulingExample/ViewModel/MainViewModel.vb
@@ -1,26 +1,30 @@
-﻿#Region "#viewmodel"
-Imports System
+#Region "#viewmodel"
 Imports System.Collections.ObjectModel
 
 Namespace SchedulerViewsExample
+
     Public Class MainViewModel
-        Public Overridable Property Doctors() As ObservableCollection(Of Doctor)
-        Public Overridable Property Appointments() As ObservableCollection(Of MedicalAppointment)
+
+        Public Overridable Property Doctors As ObservableCollection(Of Doctor)
+
+        Public Overridable Property Appointments As ObservableCollection(Of MedicalAppointment)
 
         Protected Sub New()
             CreateDoctors()
             CreateMedicalAppointments()
         End Sub
+
         Private Sub CreateDoctors()
             Doctors = New ObservableCollection(Of Doctor)()
-            Doctors.Add(Doctor.Create(Id:=1,Name:="Stomatologist"))
+            Doctors.Add(Doctor.Create(Id:=1, Name:="Stomatologist"))
             Doctors.Add(Doctor.Create(Id:=2, Name:="Ophthalmologist"))
             Doctors.Add(Doctor.Create(Id:=3, Name:="Surgeon"))
         End Sub
+
         Private Sub CreateMedicalAppointments()
             Appointments = New ObservableCollection(Of MedicalAppointment)()
-            Appointments.Add(MedicalAppointment.Create(StartTime := Date.Now.Date.AddHours(10), EndTime := Date.Now.Date.AddHours(11), DoctorId := 1, Notes := "", Location := "101", PatientName := "Dave Murrel", InsuranceNumber := "111111111", FirstVisit := True))
+            Appointments.Add(MedicalAppointment.Create(StartTime:=Date.Now.Date.AddHours(10), EndTime:=Date.Now.Date.AddHours(11), DoctorId:=1, Notes:="", Location:="101", PatientName:="Dave Murrel", InsuranceNumber:="111111111", FirstVisit:=True))
         End Sub
     End Class
 End Namespace
-#End Region ' #viewmodel
+#End Region  ' #viewmodel
